6a70918736f8ac4d20acd88fd959cae84a34a8aa
akash-infra <int.akashbca41@gmail.com>
2026-02-06 11:30:47 +0000

    Learned variables & commands output storing in a variable

---
+++ Learning-scripts/variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo " Here we are Learning Bash Scripting"
+fname="Akash"
+readonly lname="Sharma"
+age=24
+echo "Hii! My name is $fname and my surname is $lname."
+ DATE=$(date)
+echo "Today is $DATE "
+HOSTNAME=$(hostname)
+echo " The machine you are using is $HOSTNAME "
+fname="Vikash"
+lname="Vashishth"
+echo " My freind's name  is $fname $lname."
